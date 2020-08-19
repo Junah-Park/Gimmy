98fabbba321a14120703eb12e7968500fe6bc000
Junah Park <junahp@hotmail.com>
2020-08-18 19:29:13 -0700

    node.js setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express');
+const app = express();
+
+// Set up static files
+app.use(express.static('public'));
+// app.use('/styles', express.static(path.join(dirname, 'public/styles')));
+// app.use('/scripts', express.static(path.join(dirname, 'public/scripts')));
+// app.use('/views', express.static(path.join(__dirname, 'public/views')));
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+// });
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('/public/index.html');
+})
