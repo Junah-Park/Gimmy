5195b0edb7e75176d0aebc4fb84526881944d3b5
Junah Park <junahp@hotmail.com>
2020-08-21 21:02:44 -0700

    button to change text

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const app = express();
 
 // Set up static files
 app.use(express.static('public'));
+app.use(express.static('public/scripts'));
 // app.use('/styles', express.static(path.join(dirname, 'public/styles')));
-// app.use('/scripts', express.static(path.join(dirname, 'public/scripts')));
+// app.use('/scripts', express.static(path.join(__dirname, 'public/scripts')));
 // app.use('/views', express.static(path.join(__dirname, 'public/views')));
 // const server = http.createServer((req, res) => {
 //   res.statusCode = 200;
@@ -22,4 +23,9 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.sendFile('/public/index.html');
-})
+});
+
+
+// app.get('private/scripts.js', (req, res) => {
+//   res.sendFile('/private/scripts.js');
+// });
---
+++ public/scripts/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#button1").click(function(){
+        $("#testText").css({"color": "red"});
+    });
+});
+
+
+
+// document.getElementById("button1").onclick = function() {
+//     document.body.firstElementChild.style.color="red";
+// }
